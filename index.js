8960226beb0dec59b63f4b33c78e10854279db80
Daniel Ángel <angel@webcat.app>
2022-07-12 10:50:06 -0500

    Update explanation

--- index.js
+++ index.js
@@ -42,16 +42,16 @@ $t.answer(1, async () => {
 
 /*
 2. Asynchronous programming: 
-  1. First get the list of ids from the asynchronous function $source.getIds()
-  2. Then, for every id call the function $source.getText(id) to get its text
+  1. First get the list of ids from the async function $source.getIds()
+  2. Then, for every id call the async function $source.getText(id) to get its text
   3. Finally, return the list of resulting texts as an array.
     
 */
 const $source = $t.source(2)
 $t.answer(2, async () => {
     // Your code goes here:
-    // 1. Get ids
-    // 2. Get text for every id
+    // 1. Get ids: $source.getIds()
+    // 2. Get text for every id: $source.getText(id)
     // 3. Return array of texts
     return 
 })
