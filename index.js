04d43b2ca97ac6b9f490f33973698674c0114678
William Salas <willbullet52@gmail.com>
2023-03-03 18:50:29 -0500

    test done

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ However, if you have any questions, please send us an email
 to support@webcat.app with the subject "Jr Fullstack Test Questions"
 */
 
-import $t from './libs/test.js'
+import $t from "./libs/test.js";
 
 /*
 1. Data manipulation:
@@ -33,12 +33,44 @@ import $t from './libs/test.js'
   Hint: Use native array methods as well as
     Lodash(https://lodash.com/docs) modules.
 */
-import _ from 'lodash'
-const source = $t.source(1)
+import _ from "lodash";
+const source = $t.source(1);
 $t.answer(1, async () => {
-  // Your code goes here
-  return 
-})
+	// Your code goes here
+
+	let balance = 0;
+	let income = 0;
+	let expenses = 0;
+	let byCategories = {};
+
+	source.forEach((transaction) => {
+		if (transaction.type === "income") {
+			balance += transaction.amount;
+			income += transaction.amount;
+		} else {
+			balance -= transaction.amount;
+			expenses += transaction.amount;
+		}
+
+		// Add to byCategories object
+		if (!byCategories[transaction.category]) {
+			byCategories[transaction.category] = 0;
+		}
+
+		if (transaction.type === "income") {
+			byCategories[transaction.category] += transaction.amount;
+		} else {
+			byCategories[transaction.category] -= transaction.amount;
+		}
+	});
+	const targetData = {
+		balance,
+		income,
+		expenses,
+		byCategories,
+	};
+	return targetData;
+});
 
 /*
 2. Asynchronous programming: 
@@ -47,11 +79,19 @@ $t.answer(1, async () => {
   3. Finally, return the list of resulting texts as an array.
     
 */
-const $source = $t.source(2)
+const $source = $t.source(2);
 $t.answer(2, async () => {
-    // Your code goes here:
-    // 1. Get ids: $source.getIds()
-    // 2. Get text for every id: $source.getText(id)
-    // 3. Return array of texts
-    return 
-})
+	// Your code goes here:
+	// 1. Get ids: $source.getIds()
+	// 2. Get text for every id: $source.getText(id)
+	// 3. Return array of texts
+	let id = await $source.getIds();
+
+	let texts = await Promise.all(
+		id.map(async (index) => {
+			return await $source.getText(index);
+		})
+	);
+
+	return texts;
+});
